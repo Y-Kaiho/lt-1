4c86c4c6d75ba08cdc908e477235252395d5aa52
Y-Kaiho <tailangjiuhuo64@gmail.com>
2024-08-03 15:04:00 +0900

    8－2課題2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://app.diagrams.net/#G1R9_DKMIKJC6dEATPrEbHeBT79K5Kva4m#%7B%22pageId%22%3A%222cc2dc42-3aac-f2a9-1cec-A%22%7D" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <div class='post'>
+                <h2 class='title'>Title</h2>
+                <p class='body'>This is a sample body.</p>
+            </div>
+        </div>
+    </body>
+</html>
